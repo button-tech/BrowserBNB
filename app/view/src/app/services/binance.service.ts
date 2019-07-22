f65e8e6025174be875522a441c9ea8cd1787c712
Nick Kozlov <nk@buttonwallet.com>
2019-07-22 14:44:30 +0300

    recompile binance + add send Transaction

--- app/view/src/app/services/binance.service.ts
+++ app/view/src/app/services/binance.service.ts
@@ -1,6 +1,9 @@
 import {Injectable} from '@angular/core';
 import * as Binance from '../../assets/binance/bnbSDK.js';
 import {getAddressFromPrivateKey} from './binance-crypto';
+import {timer} from "rxjs";
+import {map, shareReplay, switchMap} from "rxjs/operators";
+import {HttpClient} from '@angular/common/http'
 
 @Injectable({
     providedIn: 'root'
@@ -20,7 +23,7 @@ export class BinanceService {
         'TESTNET_ATLANTIC': 'https://testnet-dex-atlantic.binance.org'
     };
 
-    constructor() {
+    constructor(private http: HttpClient) {
         this.binanceInstance = Binance.initBNB();
         this.binanceClient = this.initClient(this.networksList.MAINNET);
     }
@@ -51,9 +54,25 @@ export class BinanceService {
     }
 
     getBalance(address: string) {
+        const obv = timer(0, 60000).pipe(
+            switchMap(() => {
+                return this.http.get(`${this.networksList.MAINNET}/api/v1/account/${address}`);
+            }),
+            map((resp: any) => !resp.length ? 0 : BinanceService.getBnbBalance(resp)),
+            shareReplay(1)
+        );
     }
 
+    static getBnbBalance(resp: any) {
+        const bnb = resp.find((x) => x.symbol === 'BNB');
+        return bnb ? bnb.free : 0;
+    };
+
     getBalanceOfCoin(address: string, coin: string) {
+    }
+
+    getFiatBalance(coin: string, fiatCoin: string) {
+
     }
 
     getTransactionsHistory(address: string) {
