59751dadc0dff02704f9e21d414e655b5eb50a0f
artem <artall64@gmail.com>
2019-07-24 19:25:37 +0300

    Qickfix for address input highlight. done without usage of normal way of mat-forms

--- app/view/src/app/address-input/address-input.component.ts
+++ app/view/src/app/address-input/address-input.component.ts
@@ -1,28 +1,25 @@
-import {Component, OnInit} from '@angular/core';
-import {validateAddress} from '../services/binance-crypto'
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import {validateAddress} from '../services/binance-crypto';
 
 @Component({
     selector: 'app-address-input',
     templateUrl: './address-input.component.html',
     styleUrls: ['./address-input.component.css']
 })
-export class AddressInputComponent implements OnInit {
+export class AddressInputComponent {
 
-    currentAddress: string;
+    isValid: boolean;
+    touched: boolean;
+
+    // @ts-ignore
+    @ViewChild('addressElem') addressElem: ElementRef;
 
     constructor() {
     }
 
-    ngOnInit() {
-        this.currentAddress= (((document.getElementById('address') as HTMLInputElement).value) as unknown as string)
-    }
-
     validate() {
-        const address = (((document.getElementById('address') as HTMLInputElement).value) as unknown as string);
-        if (validateAddress(address)) {
-            document.getElementById('border').style.border = '1px solid #cbcbcb;';
-        } else {
-            (document.getElementById('border')as HTMLElement).style.border = '1 px solid rgb(207, 6, 6)';
-        }
+        const addressValue = (this.addressElem.nativeElement as HTMLInputElement).value;
+        // TODO: give Solid name
+        this.isValid = validateAddress(addressValue);
     }
 }
