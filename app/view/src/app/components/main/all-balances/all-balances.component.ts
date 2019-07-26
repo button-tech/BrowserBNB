4fdb15805b911158972723c5d6ea4d9df7af7991
Nick Kozlov <enormousrage@gmail.com>
2019-07-26 18:12:44 +0300

    Add tokens component

---
+++ app/view/src/app/components/main/all-balances/all-balances.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {BinanceService} from "../../../services/binance.service";
+import {StorageService} from "../../../services/storage.service";
+import {Observable} from "rxjs";
+import {Location} from "@angular/common";
+
+@Component({
+    selector: 'app-all-balances',
+    templateUrl: './all-balances.component.html',
+    styleUrls: ['./all-balances.component.css'],
+})
+export class AllBalancesComponent implements OnInit, OnDestroy {
+    x: Observable<any>;
+
+    constructor(private bncService: BinanceService, private storage: StorageService, private location: Location,) {
+        this.x = this.bncService.getBalance('bnb1rgsk5024ej3tg77qahty0a2kav72mr702r349w', ' https://dex.binance.org/');
+    }
+
+    goBack() {
+        this.location.back();
+    }
+
+    ngOnInit() {
+    }
+
+    ngOnDestroy() {
+    }
+
+}
