4712a73da05e0676cd34571e12a67fdf1eb9326c
artem <artall64@gmail.com>
2019-07-30 22:39:24 +0300

    Merge branch 'master' of https://github.com/button-tech/BrowserBNB into state.serivce
    # Conflicts:
    #	app/view/src/app/app.module.ts
    #	app/view/src/app/components/main/all-balances/all-balances.component.ts

--- app/view/src/app/services/binance.service.ts
+++ app/view/src/app/services/binance.service.ts
@@ -46,10 +46,6 @@ export class BinanceService {
 
     getBalance$(address: string, endpoint: string): Observable<any> {
         return this.http.get(`${endpoint}api/v1/account/${address}`).pipe(
-            // TODO: fix with share replay upstream
-            // tap((x) => {
-            //     debugger;
-            // }),
             catchError((error: HttpErrorResponse) => {
                 // TODO: properly handle binance 404 response
                 // const errResp:  AccountInfo;
