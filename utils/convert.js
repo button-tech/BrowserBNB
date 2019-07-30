538b7bf3a6ce7022bdb43b36ef0060accd2c73db
Nick Kozlov <nk@buttonwallet.com>
2019-07-30 21:29:37 +0300

    add tokens all fixes

--- utils/convert.js
+++ utils/convert.js
@@ -3,3 +3,12 @@ const BNB = require("@binance-chain/javascript-sdk");
 module.exports = {
     BNB:BNB
 };
+/// to convert from https://explorer.binance.org/api/v1/assets?page=1&rows=3500
+/// rawList.forEach((x) => {
+// 	fff.push({
+// 		"image": x.assetImg,
+// 		"symbol": x.asset,
+// 		"name": x.name,
+// 		"mappedAsset": x.mappedAsset
+// 	})
+// })
