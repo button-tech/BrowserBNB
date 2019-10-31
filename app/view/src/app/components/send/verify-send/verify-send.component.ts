e5076f9fcb6003cf639220edd8109798ec389ca9
artem <artall64@gmail.com>
2019-10-31 14:00:02 +0200

    comments and dependencies cleanup

--- app/view/src/app/components/send/verify-send/verify-send.component.ts
+++ app/view/src/app/components/send/verify-send/verify-send.component.ts
@@ -1,9 +1,4 @@
 import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
-import {BehaviorSubject, combineLatest, from, Observable} from 'rxjs';
-import {BinanceService} from '../../../services/binance.service';
-import {map, shareReplay, switchMap, take} from 'rxjs/operators';
-import {ITransaction, StateService} from '../../../services/state.service';
-import {Router} from "@angular/router";
 
 @Component({
     selector: 'app-verify-send',
@@ -40,88 +35,4 @@ export class VerifySendComponent {
     get IsBnb(): boolean {
         return this.selectedToken === 'BNB';
     }
-
-    // ngOnInit() {
-    //     const txSnapshot = this.stateService.currentTransaction.getValue();
-    //     this.tx.next(txSnapshot);
-    //
-    //     const selectedNetwork$ = this.stateService.selectedNetwork$;
-    //
-    //     selectedNetwork$.subscribe(() => {
-    //         const oldVal = this.iterator.getValue();
-    //         if (oldVal > 0) {
-    //             this.iterator.next(0);
-    //             this.router.navigate(['/send']);
-    //         } else {
-    //             this.iterator.next(oldVal + 1);
-    //         }
-    //     });
-    //
-    //     const bnb2usdRate$ = this.stateService.bnb2fiatRate$;
-    //     const bnbTransferFee$ = this.stateService.simpleFee$;
-    //
-    //     this.txDetails = combineLatest([bnb2usdRate$, bnbTransferFee$]).pipe(
-    //         map((x: any[]) => {
-    //             const [rate, fee] = x;
-    //
-    //             const tx = this.tx.getValue();
-    //             const inUSD = tx.IsAmountEnteredInUSD;
-    //             const sumInCrypto = !inUSD ? tx.Amount : tx.Amount / rate;
-    //             const sumInFiat = !inUSD ? (tx.Amount * rate) : tx.Amount;
-    //
-    //             const totalSumInTokenIfNotBNB = sumInCrypto + ' ' + tx.mappedName + ' and ' + fee.toString() + ' BNB';
-    //             const totalSumInTokenIfBNB = Number(sumInCrypto) + Number(fee);
-    //
-    //             const isBnb = tx.Symbol === 'BNB';
-    //             const totalSumInToken = isBnb
-    //                 ? totalSumInTokenIfBNB.toString()
-    //                 : totalSumInTokenIfNotBNB;
-    //
-    //             const TotalFiatSum = +sumInFiat + (Number(fee) * Number(rate));
-    //             const txDetails: ITransactionDetails = {
-    //                 IsBnb: isBnb,
-    //                 CryptoName: isBnb ? 'BNB' : tx.mappedName,
-    //                 SumInCrypto: (+sumInCrypto).toFixed(4),
-    //                 SumInCryptoRaw: sumInCrypto,
-    //                 SumInFiat: (+sumInFiat).toFixed(2),
-    //
-    //                 FeeInBNB: fee.toString(),
-    //                 FeeInFiat: (Number(fee) * Number(rate)).toFixed(2),
-    //
-    //                 TotalSumInToken: (+totalSumInToken).toFixed(4),
-    //                 TotalSumInFiat: TotalFiatSum.toFixed(2),
-    //             };
-    //             return txDetails;
-    //         }),
-    //         shareReplay(1)
-    //     );
-    // }
-    //
-    // verify() {
-    //
-    //     this.txDetails.pipe(
-    //         take(1),
-    //         switchMap((txDetails: ITransactionDetails) => {
-    //             const tx = this.tx.getValue();
-    //             const network = this.stateService.selectedNetwork$.getValue();
-    //             const privateKey = this.stateService.uiState.currentAccount.privateKey;
-    //
-    //
-    //             const p$ = this.bncService.sendTransaction(
-    //                 +txDetails.SumInCrypto,
-    //                 tx.AddressTo,
-    //                 network.label,
-    //                 network.val,
-    //                 network.networkPrefix,
-    //                 tx.Symbol,
-    //                 privateKey,
-    //                 tx.Memo
-    //             );
-    //
-    //             return from(p$);
-    //         })
-    //     ).subscribe((result) => {
-    //         console.log(result);
-    //     });
-    // }
 }
